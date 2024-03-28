6ba192c09181b1c02e187e8a41dfea3b4014abcc
Kyle <nonplayercontrolled@gmail.com>
2024-03-28 10:19:07 -0400

    added and secured API endpoints for register and login

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('express-jwt'); 
+
+var auth = jwt({   // Lab 6
+  secret: process.env.JWT_SECRET,
+  userProperty: 'payload'
+});
+
 var ctrlBlogList = require('../controllers/blogs');
+var ctrlAuth = require('../controllers/authentication');  // Lab 6
 
-//var ctrlHome = require('../controllers/home');
-//router.get('/', ctrlHome.home);
-
+/*API Routes*/
 router.get('/blogs', ctrlBlogList.blogsList);
-router.post('/blogs',ctrlBlogList.blogsCreate);
+router.post('/blogs',auth,ctrlBlogList.blogsCreate); // lab 6
 router.get('/edit/:blogId',ctrlBlogList.blogsReadOne);
-router.put('/edit/:blogId',ctrlBlogList.blogsUpdateOne);
-router.delete('/delete/:blogId',ctrlBlogList.blogsDeleteOne);
+router.put('/edit/:blogId',auth,ctrlBlogList.blogsUpdateOne); // lab 6
+router.delete('/delete/:blogId',auth,ctrlBlogList.blogsDeleteOne); // lab 6
+router.post('/register',ctrlAuth.register);
+router.post('/login',ctrlAuth.login);
 
 module.exports = router;
